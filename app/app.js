93772946d03e8d21b4b689ae84019688bfd21dce
Weder Ribas <wederribas@gmail.com>
2018-06-06 21:21:18 -0300

    Create project initial structure with Knockout and Google Maps API

---
+++ app/app.js
@@ -0,0 +1,3 @@
+function AppViewModel() {}
+
+ko.applyBindings(new AppViewModel());
---
+++ app/map.js
@@ -0,0 +1,6 @@
+function initMap() {
+  const map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 43.675, lng: -79.393 },
+    zoom: 12
+  });
+}
