fec5af76dad228183891c4b828ffffdd0107999b
Weder Ribas <wederribas@gmail.com>
2018-06-08 10:09:35 -0300

    Create object containing map selected locations

---
+++ app/locations.js
@@ -0,0 +1,50 @@
+const locations = [
+  {
+    id: 0,
+    name: "Art Gallery of Ontario",
+    lat: 43.6535147,
+    lng: -79.3927528
+  },
+  {
+    id: 1,
+    name: "Casa Loma",
+    lat: 43.6780642,
+    lng: -79.4094638
+  },
+  {
+    id: 2,
+    name: "Centreville There Park",
+    lat: 43.620502,
+    lng: -79.3736291
+  },
+  {
+    id: 3,
+    name: "CN Tower",
+    lat: 43.6425603,
+    lng: -79.3870213
+  },
+  {
+    id: 4,
+    name: "High Park Nature Centre",
+    lat: 43.6512562,
+    lng: -79.4635576
+  },
+  {
+    id: 5,
+    name: "Hockey Hall of Fame",
+    lat: 43.647259,
+    lng: -79.3776803
+  },
+  {
+    id: 6,
+    name: "Ontario Science Centre",
+    lat: 43.7163567,
+    lng: -79.339171
+  },
+  {
+    id: 7,
+    name: "Yorkdale Shopping Centre",
+    lat: 43.7254335,
+    lng: -79.4520844
+  }
+];
